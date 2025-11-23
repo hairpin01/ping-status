3ffeed6f2d9a638600e04fa2f7947784ab3a1812
Шмэлька <alichka240784@gmail.com>
2025-11-23 23:17:21 +1000

    Update crypto-prices.plugins.py

--- plugins/crypto-prices.plugins.py
+++ plugins/crypto-prices.plugins.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 __min_version__ = "3.3.0"
-
+__version__ = "1.0.1"
+__plugin_url__ = "https://raw.githubusercontent.com/hairpin01/ping-status/refs/heads/main/plugins/crypto-prices.plugins.py"
+__name__ = "crypto-prices"
 def get_help():
     return """
 Crypto Prices Plugin
